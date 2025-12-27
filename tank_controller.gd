b7658b20b7d2bbf43fb655be38cf97951c5a5417
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-12-26 23:01:58 -0500

    Fix force positioning error

--- tank_controller.gd
+++ tank_controller.gd
@@ -4,12 +4,14 @@ extends RigidBody3D
 
 @export_category("Control")
 @export var drag: float = 50.0
+@export var drive_force: float = 400.0
+@export var turn_torque: float = 100.0
 
 @export_category("Suspension")
 ## How much the suspension forces oppose compression.
-@export var stiffness: float = 1500.0
+@export var stiffness: float = 30.0
 ## How much vertical velocity (at the point of suspension) is diffused.
-@export var dampening: float = 150.0
+@export var dampening: float = 2.0
 
 var suspension_rays: Array[Node]
 
@@ -28,16 +30,30 @@ func _process(delta: float) -> void:
 	
 	if grounded:
 		
-		# apply lateral drag
+		var up      :=  global_basis.y
 		var forward := -global_basis.z
 		var right   :=  global_basis.x
 		
+		# controls
+		if Input.is_action_pressed("forward"):
+			apply_force(forward * drive_force * delta, -up * 0.2)
+		
+		if Input.is_action_pressed("backward"):
+			apply_force(-forward * drive_force * delta, -up * 0.2)
+		
+		if Input.is_action_pressed("left"):
+			apply_torque(up * turn_torque * delta)
+			
+		if Input.is_action_pressed("right"):
+			apply_torque(up * -turn_torque * delta)
+		
+		# apply lateral drag
 		var lateral_velocity := forward * linear_velocity.dot(forward) + right * linear_velocity.dot(right)
 		
 		apply_central_force(-lateral_velocity * drag * delta)
 		
 		# apply angular drag
-		apply_torque(global_basis.y * -angular_velocity.dot(global_basis.y) * drag * delta)
+		apply_torque(up * -angular_velocity.dot(up) * drag * delta)
 	
 func _apply_suspension(ray: RayCast3D) -> bool:
 	
@@ -55,13 +71,14 @@ func _apply_suspension(ray: RayCast3D) -> bool:
 		var suspension_force := compression_fac * stiffness
 		
 		# dampening opposes vertical velocity at the raycast's origin
-		var velocity_at_position := linear_velocity + angular_velocity.cross(ray.position)
+		var velocity_at_position := linear_velocity + angular_velocity.cross(ray.global_position - global_position)
 		var vertical_velocity_at_position = velocity_at_position.dot(up)
 		
 		suspension_force -= vertical_velocity_at_position * dampening
 		
 		# apply force
-		apply_force(suspension_force * up, ray.position)
+		apply_force(suspension_force * up, ray.global_position - global_position)
 		
 		return true
+		
 	return false

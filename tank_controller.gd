5f86ebcef8c7bd27864c7c92e618e62e9ab87ec0
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-12-29 04:43:44 -0500

    Combine camera controller into tank controller

--- tank_controller.gd
+++ tank_controller.gd
@@ -1,7 +1,15 @@
 extends RigidBody3D
 
-# TODO rotating camera rig that's always upright; turret attempts to face
-# towards camera direction
+# TODO turret attempts to face towards camera direction
+
+@export_category("Camera")
+@export var mouse_sensitivity: float = 0.003
+
+var camera_pitch := 0.0
+var camera_yaw := 0.0
+var camera_global_target_angle := 0.0
+
+var prev_tank_yaw := 0.0
 
 @export_category("Control")
 ## How much drag is applied when grounded (regardless of driving).
@@ -20,6 +28,8 @@ extends RigidBody3D
 var suspension_rays: Array[Node]
 
 func _ready() -> void:
+	
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 	suspension_rays = find_children("*", "RayCast3D", false)
 
@@ -59,6 +69,22 @@ func _process(delta: float) -> void:
 		# apply angular drag
 		apply_torque(up * -angular_velocity.dot(up) * drag * delta)
 	
+	# camera
+	camera_global_target_angle += global_rotation.y - prev_tank_yaw
+	prev_tank_yaw = global_rotation.y
+	
+	camera_yaw = lerp_angle(camera_yaw, camera_global_target_angle, delta * 12.0)
+	
+	# ensure camera is always above the tank (even if tipped over) and
+	# (generally) facing where the tank is facing
+	$CameraPivot.global_rotation = Vector3(camera_pitch, camera_yaw, 0)
+	
+	# speed fac
+	var speed_fac = pow(min(linear_velocity.length() * 0.05, 1.0), 0.8)
+	
+	# FOV (60 - 100)
+	$CameraPivot/Camera3D.fov = 60 + speed_fac * 40
+	
 func _apply_suspension(ray: RayCast3D) -> bool:
 	
 	ray.global_rotation = Vector3.ZERO
@@ -86,3 +112,14 @@ func _apply_suspension(ray: RayCast3D) -> bool:
 		return true
 		
 	return false
+
+func _input(event: InputEvent) -> void:
+	
+	if event is InputEventMouseMotion:
+		
+		camera_pitch = clampf(camera_pitch - event.relative.y * mouse_sensitivity, -PI / 4, PI / 8)
+		camera_global_target_angle += -event.relative.x * mouse_sensitivity
+	
+	if event.is_action_pressed("pause"):
+		
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED else Input.MOUSE_MODE_CAPTURED)
